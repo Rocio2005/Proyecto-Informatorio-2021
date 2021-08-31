3b72246508be77bf20183818bd36ab101eefa1a4
Rocio2005 <rochi1605@gmail.com>
2021-08-30 22:30:26 -0300

    IN03-Agregado Servicio de Modificar Producto

--- main/java/com/informatorio/Proyecto/Final/controller/ProductoController.java
+++ main/java/com/informatorio/Proyecto/Final/controller/ProductoController.java
@@ -1,6 +1,7 @@
 package com.informatorio.Proyecto.Final.controller;
 
 import com.informatorio.Proyecto.Final.domain.Producto;
+import com.informatorio.Proyecto.Final.domain.Usuario;
 import com.informatorio.Proyecto.Final.repository.ProductoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,22 @@ public class ProductoController {
     public ResponseEntity<?> borrarUsuario(@PathVariable Long id){
         productoRepository.deleteById(id);
         return new ResponseEntity<>("Producto "+id+ " eliminado",HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")/*MODIFICAR PRODUCTO*/
+    public ResponseEntity<?>  modificarProducto(@PathVariable Long id,@RequestBody Producto producto){
+        Producto prodAmodificar=productoRepository.findById(id).get();
+
+        prodAmodificar.setNombre(producto.getNombre());
+        prodAmodificar.setDescripcion(producto.getDescripcion());
+        prodAmodificar.setPrecioUnitario(producto.getPrecioUnitario());
+        prodAmodificar.setContenido(producto.getContenido());
+        prodAmodificar.setPublicado(producto.getPublicado());
+
+        productoRepository.save(prodAmodificar);
+
+        return new ResponseEntity<>("Producto " +id+" Modificado",HttpStatus.OK);
+
     }
 }
 
